80e4d927d24ce491251334110633fea77a7b4263
Mc Donald Fonyo <mcdonaldfonyo@outlook.com>
2020-06-21 22:06:54 +0200

    Application Data Source used

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyConsole.Models;
+using MyConsole.Repository;
 
 namespace MyConsole.Controllers
 {
     public class BookController : Controller
     {
-        public string getAllBooks()
+
+        private readonly BookRepository bookRepository=null;
+
+        public BookController()//type ctr to get the deafult constructor
         {
-            return "All books";
+            bookRepository = new BookRepository();  
+        }
+        //at first we had a string type, but cz we are dealing with a list type inside the method, then we change the return type to List
+        public List<BookModel> getAllBooks()
+        {
+            return bookRepository.GetAllBooks();
+        }
+        public BookModel GetBook(int id)
+        {
+            return bookRepository.GetBookById(id);
+        }
+        public List<BookModel> SearchBooks(string bookName,string authorName)
+        {
+            return bookRepository.SearchBook(bookName, authorName);
         }
     }
 }
---
+++ Repository/BookRepository.cs
@@ -0,0 +1,39 @@
+﻿using MyConsole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyConsole.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();//the Where method has a condition to filter the by ID.
+        }
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id=0, Title="My Life is of God", Author="Jesus "},
+                new BookModel(){ Id=2, Title="My Goals", Author="Mc Donald"},
+                new BookModel(){ Id=3, Title="My Career", Author="Mc Donald"},
+                new BookModel(){ Id=1, Title="My Life Plans", Author="Jesus "},
+                new BookModel(){ Id=4, Title="Strategies", Author="Mc Donald"},
+                new BookModel(){ Id=5, Title="Personality Type", Author="Mc Donald"}
+            };
+        
+        }
+
+
+    }
+}
